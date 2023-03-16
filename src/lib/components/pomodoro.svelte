de25db21b91508b9d98c88521f55949e62f79728
spences10 <spences10apps@gmail.com>
2023-03-16 08:20:51 +0000

    refactor: :recycle: add milliseconds option

--- src/lib/components/pomodoro.svelte
+++ src/lib/components/pomodoro.svelte
@@ -5,6 +5,8 @@
 	let time_left = 25 * 60;
 	let timer_running = false;
 	let interval: ReturnType<typeof setTimeout>;
+	let show_milliseconds = false;
+	let milliseconds = 0;
 
 	const pad_time = (time: number) => {
 		return time.toString().padStart(2, '0');
@@ -17,9 +19,20 @@
 		if (timer_running) return;
 
 		interval = setInterval(() => {
-			time_left = time_left >= 1 ? time_left - 1 : 0;
-			if (time_left === 0) reset_timer();
-		}, 1000);
+			if (show_milliseconds) {
+				milliseconds -= 10;
+				if (milliseconds <= 0) {
+					milliseconds = 990;
+					time_left -= 1;
+				}
+			} else {
+				time_left -= 1;
+			}
+
+			if (time_left <= 0) {
+				reset_timer();
+			}
+		}, show_milliseconds ? 10 : 1000);
 		timer_running = true;
 	};
 
@@ -55,8 +68,18 @@
 	<span>{minutes}</span>
 	<span>:</span>
 	<span>{seconds}</span>
+	{#if show_milliseconds}
+		<span>:</span>
+		<span>{pad_time(Math.floor(milliseconds / 10))}</span>
+	{/if}
 </div>
 <div>
 	<button on:click={toggle_timer}>{toggle_button_text}</button>
 	<button on:click={reset_timer}>Reset</button>
+</div>
+<div>
+	<label>
+		<input type="checkbox" bind:checked={show_milliseconds} />
+		Show milliseconds
+	</label>
 </div>
